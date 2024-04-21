bd4117ac607b185debb2d359ee3382a7373e6033
nguyensyminhtoan <vophingoc1995@gmail.com>
2024-04-21 16:09:43 +0700

    update origin cors backend

--- Backend/app.js
+++ Backend/app.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const movieRouter=require('./routes/movie')
+const movieRouter = require('./routes/movie')
 const authMiddleware = require('./middleware/authMiddleware')
-app.use(cors())
+app.use(cors({
+  origin: "https://movie-keqy.onrender.com/"
+}))
 app.use(express.json())
 //middleware để xác thực người dùng
 app.use(authMiddleware)
 
 app.use(movieRouter)
-app.use((req,res)=>{
-  res.status(404).json({message:"Route not found"})
+app.use((req, res) =>
+{
+  res.status(404).json({ message: "Route not found" })
 })
 app.listen(5000)
