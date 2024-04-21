85ec17aa90b60f53a1ed36dcad7c042a63bd8e4e
nguyensyminhtoan <vophingoc1995@gmail.com>
2024-04-21 16:26:13 +0700

    update backend

--- Backend/model/UserToken.js
+++ Backend/model/UserToken.js
@@ -1,21 +1,24 @@
 const fs = require('fs');
 const path = require('path');
 
-const DATA_PATH = path.join(path.dirname(process.mainModule.filename),'data','userToken.json')
+const DATA_PATH = path.join(__dirname, '..', 'data', 'userToken.json')
 
 const UserToken = {
-  all: function() {
+  all: function ()
+  {
     return JSON.parse(fs.readFileSync(DATA_PATH, 'utf8'));
   },
 };
 
-const authModel=(token)=>{
-  const userTokenList=UserToken.all()
+const authModel = (token) =>
+{
+  const userTokenList = UserToken.all()
   //Kiểm tra xem token có trong danh sách không
-  const user=userTokenList.find((user)=>user.token===token)
-  if(!user){
-    return {error:401,message:'Unauthorized'}
+  const user = userTokenList.find((user) => user.token === token)
+  if (!user)
+  {
+    return { error: 401, message: 'Unauthorized' }
   }
   return user
 }
-module.exports=authModel
+module.exports = authModel
